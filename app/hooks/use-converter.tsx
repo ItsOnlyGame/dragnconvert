5b06aa20a112ef4574063a550120c97aa4c1d3b7
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2024-11-30 17:50:45 +0200

    refactor(hooks): cleanup use-converter hook code and add comments

--- app/hooks/use-converter.tsx
+++ app/hooks/use-converter.tsx
@@ -3,7 +3,13 @@ export interface ConvertedFile {
   filename: string
 }
 
-const convertToPng = async (file: File): Promise<ConvertedFile> => {
+/**
+ * Convert a file to PNG
+ *
+ * @param file File from an <input />
+ * @returns A promise that resolves to a ConvertedFile
+ */
+const convertToPNG = async (file: File): Promise<ConvertedFile> => {
   const img = new Image()
   img.src = URL.createObjectURL(file)
 
@@ -19,46 +25,24 @@ const convertToPng = async (file: File): Promise<ConvertedFile> => {
       const filename = file.name.split('.').slice(0, -1).join('.')
 
       canvas.toBlob((blob) => {
-        if (blob)
+        if (blob) {
           resolve({
             blob: blob,
             filename: `${filename}.png`,
           })
+        }
       }, 'image/png')
     }
   })
 }
 
-// function convertToPng(inputFile: File) {
-//   const canvas = document.createElement('canvas')
-//   const ctx = canvas.getContext('2d')
 
-//   const filename = inputFile.name.split('.').slice(0, -1).join('.')
 
-//   if (!ctx) {
-//     console.error('Could not create a canvas')
-//     return
-//   }
 
-//   const img = new Image()
 
-//   img.onload = () => {
-//     canvas.width = img.width
-//     canvas.height = img.height
-//     ctx.drawImage(img, 0, 0)
-//     const pngDataUrl = canvas.toDataURL('image/png')
-
-//     const link = document.createElement('a')
-//     link.href = pngDataUrl
-//     link.download = `${filename}.png`
-//     link.click()
-//   }
-
-//   img.src = URL.createObjectURL(inputFile)
-// }
 
 export function useConverter() {
   return {
-    convertToPng,
+    convertToPNG,
   }
 }
