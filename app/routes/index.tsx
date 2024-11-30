1a895a2f76e0447a0887fe7ac8e80f9a1f7a22f9
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2024-11-30 17:52:09 +0200

    feat: create a dedicated home page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
-import { ConvertPage } from '@/components/convert-page'
 import { useConverter } from '@/hooks/use-converter'
 import { useDownload } from '@/hooks/use-download'
+import { Layout } from '@/layouts/layout'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
@@ -20,10 +20,21 @@ function Home() {
   }
 
   return (
-    <ConvertPage
-      title="JPG to PNG converter"
-      handleUpload={handleUpload}
-      fileTypes=".jpg,.jpeg"
-    />
+    <Layout>
+      <div className="flex flex-col items-center justify-center w-full h-full gap-8">
+        <div className="flex flex-col items-center justify-center gap-3">
+          <h2 className="text-3xl font-bold">Welcome to convertion land!</h2>
+
+          <p className="font-semibold">
+            Here you can convert your files from one format to another.
+          </p>
+        </div>
+
+        <p>
+          Get started by selecting on the left, what what kind of convertion you
+          want to do
+        </p>
+      </div>
+    </Layout>
   )
 }
