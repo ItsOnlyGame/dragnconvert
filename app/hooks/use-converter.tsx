ef2d92b82565d7923252f8c07b545453c5f4e97f
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2024-11-30 17:52:52 +0200

    feat: add png-to-jpg conversion function and add a page for png-to-jpg conversions

--- app/hooks/use-converter.tsx
+++ app/hooks/use-converter.tsx
@@ -36,13 +36,51 @@ const convertToPNG = async (file: File): Promise<ConvertedFile> => {
   })
 }
 
+/**
+ * Convert a file to JPEG
+ *
+ * @param file File from an <input />
+ * @param quality A number between 0.1 and 1.0
+ * @returns A promise that resolves to a ConvertedFile
+ */
+const convertToJPG = async (
+  file: File,
+  quality: number
+): Promise<ConvertedFile> => {
+  const img = new Image()
+  img.src = URL.createObjectURL(file)
 
+  return new Promise((resolve) => {
+    img.onload = () => {
+      const canvas = document.createElement('canvas')
+      canvas.width = img.width
+      canvas.height = img.height
 
+      const ctx = canvas.getContext('2d')
+      ctx?.drawImage(img, 0, 0)
 
+      const filename = file.name.split('.').slice(0, -1).join('.')
 
+      // Convert to JPEG
+      canvas.toBlob(
+        (blob) => {
+          if (blob) {
+            resolve({
+              blob: blob,
+              filename: `${filename}.jpg`,
+            })
+          }
+        },
+        'image/jpeg',
+        quality
+      ) // Quality (0.1–1.0)
+    }
+  })
+}
 
 export function useConverter() {
   return {
     convertToPNG,
+    convertToJPG,
   }
 }
