955005c145694cc7abe38fcec2d343339a4a9b83
Jaro Heinonen <33194188+ItsOnlyGame@users.noreply.github.com>
2025-07-29 00:22:41 +0300

    fix: move index inside convert-layout

--- src/routes.ts
+++ src/routes.ts
@@ -6,9 +6,10 @@ import {
 } from '@react-router/dev/routes'
 
 export default [
-  index('routes/index.tsx'),
   layout('routes/full-layout.tsx', [route('*', './routes/not-found.tsx')]),
+
   layout('routes/convert-layout.tsx', [
+    index('routes/index.tsx'),
 
     route('/to-jpg', 'routes/converters/convert-to-jpg.tsx'),
     route('/to-png', 'routes/converters/convert-to-png.tsx'),
