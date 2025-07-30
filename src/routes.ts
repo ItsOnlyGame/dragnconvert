3bbde3d3335b83fc978a273f551230863dfe417f
Jaro Heinonen <33194188+ItsOnlyGame@users.noreply.github.com>
2025-07-30 04:00:51 +0300

    misc: add comments to categorize converter routes

--- src/routes.ts
+++ src/routes.ts
@@ -11,10 +11,15 @@ export default [
   layout('routes/convert-layout.tsx', [
     index('routes/index.tsx'),
 
+    // Image
     route('/to-jpg', 'routes/converters/convert-to-jpg.tsx'),
     route('/to-png', 'routes/converters/convert-to-png.tsx'),
     route('/to-webp', 'routes/converters/convert-to-webp.tsx'),
+
+    // Video
     route('/to-mp4', 'routes/converters/convert-to-mp4.tsx'),
+
+    // Audio
     route('/to-mp3', 'routes/converters/convert-to-mp3.tsx'),
   ]),
 ] satisfies RouteConfig
